a9faf8e2ccfa8848271f5dc8f5dacac8df93357c
Alan <ananni@e-softing.com.ar>
2021-10-01 12:49:22 -0300

    Agrega exponencia y soluciona problema de ejecución de programa

--- calculos.php
+++ calculos.php
@@ -1,12 +1,18 @@
 <?php   
 
-$num1 = $_GET['n1'];
-$num2 = $_GET['n2'];
+$n1 = $_GET['n1'];
+$n2 = $_GET['n2'];
 
 $suma = $n1 + $n2;
 $resta = $n1 - $n2;
 $producto = $n1 * $n2;
-$division = $n1 / $n2;
+$exponencia = $n1 ** $n2;
+
+if ($n2 == 0){
+    $division = "No se puede dividir entre 0";
+}else{
+    $division = $n1 / $n2;
+}
 
 ?>
 
@@ -22,7 +28,8 @@ $division = $n1 / $n2;
     <?php echo "<p>La suma de $n1 + $n2 = $suma</p>";
           echo "<p>La resta de $n1 - $n2 = $resta</p>";
           echo "<p>El producto de $n1 * $n2 = $producto</p>";
-          echo "<p>La resta de $n1 / $n2 = $division</p>";
+          echo "<p>El cociente de $n1 / $n2 = $division</p>";
+          echo "<p>La exponenciación de  $n1 elevado a $n2 = $exponencia</p>";
     ?>
 </body>
 </html>
